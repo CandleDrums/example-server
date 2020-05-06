620235f582a3e48298f0bd2dae0429b9f88f6190
liming <liming@localhost>
2020-05-06 17:26:50 +0800

    修改

---
+++ example-server-deploy/src/test/java/com/cds/example/server/core/manage/test/ExampleManageServiceImplTest.java
@@ -0,0 +1,48 @@
+/**
+ * @Project example-server-deploy
+ * @Package com.cds.example.server.core.manage.test
+ * @Class ExampleManageServiceImplTest.java
+ * @Date [date]
+ * @Copyright (c) 2020 CandleDrums.com All Right Reserved.
+ */
+package com.cds.example.server.core.manage.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description Example管理Server测试
+ * @Notes 未填写备注
+ * @author [author]
+ * @Date [date]
+ */
+class ExampleManageServiceImplTest {
+
+    /**
+     * Test method for {@link com.cds.example.server.core.manage.ExampleManageServiceImpl#delete(java.lang.String)}.
+     */
+    @Test
+    void testDelete() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link com.cds.example.server.core.manage.ExampleManageServiceImpl#add(com.cds.api.example.model.ExampleVO)}.
+     */
+    @Test
+    void testAddExampleVO() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for
+     * {@link com.cds.example.server.core.manage.ExampleManageServiceImpl#modify(com.cds.api.example.model.ExampleVO)}.
+     */
+    @Test
+    void testModifyExampleVO() {
+        fail("Not yet implemented");
+    }
+
+}
---
+++ example-server-deploy/src/test/java/com/cds/example/server/core/query/test/ExampleQueryServiceImplTest.java
@@ -0,0 +1,27 @@
+/**
+ * @Project example-server-deploy
+ * @Package com.cds.example.server.core.query.test
+ * @Class ExampleQueryServiceImplTest.java
+ * @Date [date]
+ * @Copyright (c) 2020 CandleDrums.com All Right Reserved.
+ */
+package com.cds.example.server.core.query.test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description Example查询Server测试
+ * @Notes 未填写备注
+ * @author [author]
+ * @Date [date]
+ */
+public class ExampleQueryServiceImplTest {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
