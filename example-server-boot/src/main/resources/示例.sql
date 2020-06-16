9c479eb76f7bcb9fc1abb5e7ab64939b335a2a9c
liming <liming@limingdembp>
2020-06-16 18:37:56 +0800

    修改

--- example-server-boot/src/main/resources/示例.sql
+++ example-server-boot/src/main/resources/示例.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `example`;
-CREATE TABLE `example` (
+DROP TABLE IF EXISTS `示例`;
+CREATE TABLE `示例` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `version` int(11) NOT NULL DEFAULT '0' COMMENT '版本号',
   `num` varchar(32) NOT NULL COMMENT '编号',
@@ -7,8 +7,8 @@ CREATE TABLE `example` (
   `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
   `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '删除标识',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `index_example_num` (`num`) USING HASH COMMENT '编码唯一性索引',
-  KEY `index_example_create_date` (`create_date`) USING BTREE COMMENT '创建时间查询索引',
-  KEY `index_example_update_date` (`update_date`) USING BTREE COMMENT '更新时间查询索引'
+  UNIQUE KEY `index_示例_num` (`num`) USING HASH COMMENT '编码唯一性索引',
+  KEY `index_示例_create_date` (`create_date`) USING BTREE COMMENT '创建时间查询索引',
+  KEY `index_示例_update_date` (`update_date`) USING BTREE COMMENT '更新时间查询索引'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='示例';
 
