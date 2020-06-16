330a5c68d6d0f710a6abeec981346fb8cb2c46cf
liming <liming@limingdembp>
2020-06-16 17:00:11 +0800

    修改

---
+++ example-server-boot/src/main/resources/example.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS `example`;
+CREATE TABLE `example` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `version` int(11) NOT NULL DEFAULT '0' COMMENT '版本号',
+  `num` varchar(32) NOT NULL COMMENT '编号',
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '删除标识',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_example_num` (`num`) USING HASH COMMENT '编码唯一性索引',
+  KEY `index_example_create_date` (`create_date`) USING BTREE COMMENT '创建时间查询索引',
+  KEY `index_example_update_date` (`update_date`) USING BTREE COMMENT '更新时间查询索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='示例';
+
